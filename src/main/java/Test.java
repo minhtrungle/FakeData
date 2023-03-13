2f8a799f43cd528e317dd6a62107832d8fe50c01
minhtrungle <minhtrungle21599@gmail.com>
2023-03-13 16:17:34 +0700

    FakeData

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Locale;
 import com.github.javafaker.Faker;
 public class Test {
-    public static void main(String[] args) {
+    public static <Faker> void main(String[] args) {
         List<Human> humanList = new ArrayList<>();
         Faker faker = new Faker(new Locale("vi"));
 
@@ -13,8 +13,15 @@ public class Test {
             human.setId(i);
             human.setFirstname(faker.name().firstName());
             human.setLastname(faker.name().lastName());
-            human.setCity(faker.address.);
+            human.setCity(faker.address().cityName());
+            human.setGender(faker.number().numberBetween(0, 2));
+            human.setAge(faker.number().numberBetween(10,51));
+            human.setSalary(faker.number().numberBetween(10,3001));
+            // Them vao humanList
+            humanList.add(human);
         }
+
+        humanList.stream().forEach(p-> System.out.println(p));
 
     }
 }
